5e898fce2a8d39de32712e0d94a110c44fbf5657
Mathieu Marques <mathieumarques78@gmail.com>
2024-03-25 22:47:51 +0100

    feat(markdown): rename readMarkdown to read

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,11 +1,11 @@
 import { readFileSync } from 'fs';
 import matter from 'gray-matter';
 
-/** Read file at `path` as Markdown and return the matter found. */
-export const readMarkdown = (
-  path: string,
-): { content: string; data: Record<string, unknown> } => {
+type Matter = Record<string, unknown>;
+
+/** Read file at `path` as Markdown and parse the frontmatter found. */
+export const read = (path: string): { matter: Matter; text: string } => {
   const buffer: string = readFileSync(path, 'utf8');
   const { content, data } = matter(buffer);
-  return { content, data };
+  return { matter: data, text: content };
 };
