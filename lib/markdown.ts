03c334faf6aec9ea20befea5bbd822c6ac5f1922
Mathieu Marques <mathieumarques78@gmail.com>
2024-03-26 01:12:09 +0100

    feat(markdown): add error handling

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,11 +1,23 @@
 import { readFileSync } from 'fs';
+import { join } from 'path';
 import matter from 'gray-matter';
 
 type Matter = Record<string, unknown>;
 
-/** Read file at `path` as Markdown and parse the frontmatter found. */
-export const read = (path: string): { matter: Matter; text: string } => {
-  const buffer: string = readFileSync(path, 'utf8');
-  const { content, data } = matter(buffer);
-  return { matter: data, text: content };
+/**
+ * Read file at `path` as Markdown and parse the frontmatter found.
+ * By default look in the `markdown/` directory where the process is run.
+ */
+export const read = (
+  path: string,
+  root: string = 'markdown',
+): { matter: Matter; text: string } => {
+  try {
+    const buffer = readFileSync(join(process.cwd(), root, path), 'utf8');
+    const { content, data } = matter(buffer);
+    return { matter: data, text: content };
+  } catch (error) {
+    const message = error instanceof Error ? error.message : `${error}`;
+    throw new Error(`${message} in "${join(root, path)}"`);
+  }
 };
