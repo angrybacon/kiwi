9a4c4c011cde39d47fb14eada3fc15367a3153f4
Mathieu Marques <mathieumarques78@gmail.com>
2024-04-07 03:20:43 +0200

    feat(read): provide safe title

--- lib/read.ts
+++ lib/read.ts
@@ -2,8 +2,6 @@ import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 import matter from 'gray-matter';
 
-type Matter = Record<string, unknown>;
-
 /**
  * Read file at `path` as Markdown and parse the frontmatter found.
  * By default look in the `markdown/` directory where the process is run.
@@ -11,11 +9,15 @@ type Matter = Record<string, unknown>;
 export const read = (
   path: string,
   root: string = 'markdown',
-): { matter: Matter; text: string } => {
+): { matter: Record<string, unknown>; text: string; title?: string } => {
   try {
     const buffer = readFileSync(join(process.cwd(), root, path), 'utf8');
     const { content, data } = matter(buffer);
-    return { matter: data, text: content };
+    const { title } = data;
+    if (title && typeof title !== 'string') {
+      throw new Error(`Wrong format for title "${title}"`);
+    }
+    return { matter: data, text: content, title };
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
     throw new Error(`${message} in "${join(root, path)}"`);
