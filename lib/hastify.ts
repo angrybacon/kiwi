eb0bced2652710ef6a6874fb712b52192b06f47b
Mathieu Marques <mathieumarques78@gmail.com>
2025-03-19 17:08:20 +0100

    fix(hastify): keep directive reference

--- lib/hastify.ts
+++ lib/hastify.ts
@@ -1,27 +1,32 @@
 type Directive = {
+  attributes?: Record<string, any> | null;
   data?: { hName?: string; hProperties?: object };
   name: string;
 };
 
 /**
  * Augment the input directive with the provided extra properties.
- * Asserts that the output directive has the expected well-formed properties.
+ *
+ * This also asserts that the output directive has the expected well-formed
+ * properties.
+ *
+ * Note that the recommended way to decorate things seems to involve flat and
+ * HTML-supported attributes in the resulting `hProperties` map. Since we need
+ * more complex data, we cheat and use that very map to transport data from
+ * remarkers to renderers. This might make HAST gurus cringe a lot but heh it
+ * works.
  */
-export function hastify<
-  TDirective extends Directive,
-  TProperties extends object,
->(
+export function hastify<TDirective extends Directive, TExtra extends object>(
   directive: TDirective,
-  properties: TProperties,
+  extra: TExtra,
 ): asserts directive is TDirective & {
   data: NonNullable<TDirective['data']> & {
     hName: TDirective['name'];
-    hProperties: NonNullable<TDirective['data']>['hProperties'] & TProperties;
+    hProperties: NonNullable<TDirective['data']>['hProperties'] & TExtra;
   };
 } {
-  directive.data = {
-    ...directive.data,
-    hName: directive.name,
-    hProperties: { ...directive.data?.hProperties, ...properties },
-  };
+  const { attributes, data, name } = directive;
+  directive.data = data || {};
+  directive.data.hName = name;
+  directive.data.hProperties = { ...attributes, ...extra };
 }
