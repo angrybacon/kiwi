913f8eed07e0240a179fb1ff799a48069baedcd6
Mathieu Marques <mathieumarques78@gmail.com>
2025-10-21 02:12:04 +0200

    chore(prettier): migrate to TypeScript configuration file

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,11 +1,14 @@
-module.exports = {
+import { type Config } from 'prettier';
+
+export default {
   importOrder: [
     '<BUILTIN_MODULES>',
     '<THIRD_PARTY_MODULES>',
     '',
-    '^@/',
+    '^~/',
+    '',
     '^[.]',
   ],
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
   singleQuote: true,
-};
+} satisfies Config;
