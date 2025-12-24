a8af12db877ad1cfdb35b7b735b9824f42af83a5
Mathieu Marques <mathieumarques78@gmail.com>
2025-12-24 12:28:35 +0100

    fix(read): specify plugin generics (#3)
    * fix(read): specify plugin generics
    
    * feat(workflows): run lint and test in all branches

--- lib/remarkMinutes.ts
+++ lib/remarkMinutes.ts
@@ -1,9 +1,10 @@
+import { type Root } from 'mdast';
 import readingTime from 'reading-time';
 import { type Plugin } from 'unified';
 import { type Literal } from 'unist';
 import { visit } from 'unist-util-visit';
 
-export const remarkMinutes: Plugin = () => async (tree, file) => {
+export const remarkMinutes: Plugin<[], Root> = () => async (tree, file) => {
   let text = '';
   visit(tree, 'text', (node) => {
     text += (node as Literal).value;
